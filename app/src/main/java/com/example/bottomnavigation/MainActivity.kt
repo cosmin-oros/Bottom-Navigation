a340e66354093797be5d6ad7971c62e3a4566f9c
cosmin-oros <oroscosmin09@gmail.com>
2022-06-19 15:11:45 +0300

    added splash screen

--- app/src/main/java/com/example/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.bottomnavigation
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,10 +17,14 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,6 +36,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.bottomnavigation.ui.theme.BottomNavigationTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     @ExperimentalMaterialApi
@@ -73,7 +83,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "home") {
+    NavHost(navController = navController, startDestination = "splash_screen") {
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
         composable("home") {
             HomeScreen()
         }
@@ -167,5 +180,37 @@ fun SettingsScreen() {
         contentAlignment = Alignment.Center
     ) {
         Text(text = "Settings screen")
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavHostController){
+    val scale = remember{
+        Animatable(0f)
+        
+    }
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("home")
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.capybara),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value)
+        )
     }
 }
